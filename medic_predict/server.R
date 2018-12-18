7bf1620d5e88ac5fc068016116e386194a18221f
Diwash shrestha <diwash712@gmail.com>
2018-12-18 18:41:44 +0545

    Add files via upload

--- medic_predict/server.R
+++ medic_predict/server.R
@@ -1,50 +1,44 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-source("/home/diwash/Projects/Medical_Expense_Predictor/model.R")
+#Server codes
 
 library(shiny)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-   
-test <- reactive({
-  age <- as.integer(input$age)
-  sex <- as.character(input$sex)
-  bmi <- as.integer(input$bmi)
-  children <- as.integer(input$children)
-  smoker <- as.character(input$smoke)
-  region <- as.character(input$region)
-  age2 <- as.integer(input$age^2)
-  bmi30 <- as.integer(ifelse(input$bmi >= 30 ,1,0))
-
+  test <- reactive({
+    age <- as.integer(input$age)
+    sex <- as.character(input$sex)
+    bmi <- as.integer(input$bmi)
+    children <- as.integer(input$children)
+    smoker <- as.character(input$smoke)
+    region <- as.character(input$region)
+    age2 <- as.integer(input$age ^ 2)
+    bmi30 <- as.integer(ifelse(input$bmi >= 30 , 1, 0))
+    
+    
+    fest <- cbind(age, sex, bmi, children, smoker, region, age2, bmi30)
+    
+    test <-
+      cbind.data.frame(age, sex, bmi, children, smoker, region, age2, bmi30)
+    test <- as.data.frame(test)
+    
+    
+  })
   
-  fest <- cbind(age,sex,bmi,children,smoker,region,age2,bmi30)
+  data <- reactive({
+    bata <-
+      read.csv("/home/diwash/Projects/Medical_Expense_Predictor/data/insurance.csv")
+    bata$age2 <- bata$age ^ 2
+    bata$bmi30 <- ifelse(bata$bmi >= 30 , 1 , 0)
+    data <- bata
+  })
   
-  test <- cbind.data.frame(age, sex, bmi, children, smoker, region,age2,bmi30)
-  test <- as.data.frame(test)
   
-  
-})
-
-data <- reactive({
-  bata <- read.csv("/home/diwash/Projects/Medical_Expense_Predictor/data/insurance.csv")
-  bata$age2 <- bata$age^2
-  bata$bmi30 <- ifelse(bata$bmi >= 30 ,1 ,0)
-  data <- bata
-})
-
-
-pred <- eventReactive(input$predict,{
-  model <-  lm(charges~ age + age2 + children + bmi + sex + bmi30*smoker + region , data = data())
-  predict(model,test())
-})
+  pred <- eventReactive(input$predict, {
+    model <-
+      lm(charges ~ age + age2 + children + bmi + sex + bmi30 * smoker + region ,
+         data = data())
+    predict(model, test())
+  })
   output$insurance <- renderText(pred())
   output$table <- renderTable(test())
 })
-
--- medic_predict/ui.R
+++ medic_predict/ui.R
@@ -1,68 +1,117 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
+##Codes for the UI
 
-library(shiny)
+library(shiny) 
 library(shinythemes)
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(theme = shinytheme("spacelab"),
+shinyUI(fluidPage(
+  theme = shinytheme("united"),
   
   # Application title
   titlePanel("Medical Insurance Predictor"),
   
-  # Sidebar with a slider input for number of bins 
+  # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(
-      selectInput(inputId = 'smoke', label = 'Smoker' , 
-                  levels(insurance$smoker)),
+      selectInput(
+        inputId = 'smoke',
+        label = 'Smoker' ,
+        levels(insurance$smoker)
+      ),
       
-      selectInput(inputId = 'region', label = 'Select Region',
-                  levels(insurance$region)),
+      selectInput(
+        inputId = 'region',
+        label = 'Select Region',
+        levels(insurance$region)
+      ),
       
-      selectInput(inputId = 'sex', label = "Select Gender", 
+      selectInput(inputId = 'sex', label = "Select Gender",
                   levels(insurance$sex)),
       
-      numericInput(inputId = 'age', label = 'Age ',10, 
-                   min = 1),
-      numericInput(inputId = 'children', label = 'No. of Children:',10,
-                   min = 0),
-      numericInput(inputId = 'bmi', label = 'BMI(Body Mass Index)',10),
-      actionButton('predict',"Predict",icon = icon("refresh"))
+      numericInput(
+        inputId = 'age',
+        label = 'Age ',
+        30,
+        min = 1
+      ),
+      numericInput(
+        inputId = 'children',
+        label = 'No. of Children:',
+        1,
+        min = 0
+      ),
+      numericInput(inputId = 'bmi', label = 'BMI(Body Mass Index)', 20),
+      actionButton('predict', "Predict", icon = icon("refresh"))
       
     ),
     
     # Show a plot of the generated distribution
-    mainPanel(
-
-      tabsetPanel(
-        tabPanel(icon = icon("book"),title= "About",tags$div("The Goal of this data analysis is to use patient data to etimate the average mediacal care 
-                                         expenses for such population segments.These estimates can be used to create acturial tables 
-                                         that set the price of yearly premiums higher  or lower, depending on the expected treatment 
-                                         costs.For this analysis, i will use a simulated datasets containing hypothetical medical expenses
-                                         for patients in the United States.This data was created using demograpic statistics from 
-                                         the US Census Bureu and thus approximately reflect real world conditions.Features of the datsets:",
-                                         tags$br(),
-                                         tags$ul(
-                                           tags$li("age : An integer indicating the age of the primary beneficiary (excluding those above 
-                                                   64 years, since they are generally covered by the government).",
-                                                   tags$li("sex : The policy holder's gender, either male or female"),
-                                                   tags$li("bmi : The body mass index (BMI), which provides a sense of how over- or 
-                                                           under-weight a person is relative to their height. BMI is equal to weight (inkilograms) 
-                                                           divided by height (in meters) squared. An ideal BMI is within the range of 18.5 to 24.9."),
-                                                   tags$li("children : An integer indicating the number of children/dependents covered by 
-                                                           the insurance plan."),
-                                                   tags$li("smoker : A yes or no categorical variable that indicates whether the insured"),
-                                                   tags$li("region : The beneficiary's place of residence in the US, divided into four regularly 
-                                                           smokes tobacco. geographic regions: northeast, southeast, southwest, or northwest."))))),
-          
-
-        tabPanel(icon = icon("table"),"Your Data:", tableOutput('table'),tags$h4("Medical Expenses:",icon("dollar")),tags$h4(textOutput('insurance')))
+    mainPanel(tabsetPanel(
+      tabPanel(
+        icon = icon("table"),
+        "Prediction:",
+        tableOutput('table'),
+        tags$h4("Medical Expenses:", icon("dollar")),
+        tags$h4(textOutput('insurance'))
+      ),
+      
+      tabPanel(
+        icon = icon("book"),
+        title = "About",
+        tags$div(
+          "This application predict the Medical care expenses
+          using the given input from user.The Data used for this application is taken from
+          ",
+          tags$a("Kaggle.", href = "https://www.kaggle.com/mirichoi0218/insurance"),
+          tags$br(),
+          tags$br(),
+          "This data was created using demograpic statistics from
+          the US Census Bureu and thus approximately reflect real world conditions.Features of the datsets:",
+          tags$br(),
+          tags$ul(
+            tags$li(
+              "age : An integer indicating the age of the primary beneficiary (excluding those above
+              64 years, since they are generally covered by the government).",
+              tags$li("sex : The policy holder's gender, either male or female"),
+              tags$li(
+                "bmi : The body mass index (BMI), which provides a sense of how over- or
+                under-weight a person is relative to their height. BMI is equal to weight (inkilograms)
+                divided by height (in meters) squared. An ideal BMI is within the range of 18.5 to 24.9."
+              ),
+              tags$li(
+                "children : An integer indicating the number of children/dependents covered by
+                the insurance plan."
+              ),
+              tags$li(
+                "smoker : A yes or no categorical variable that indicates
+                whether the insured"
+              ),
+              tags$li(
+                "region : The beneficiary's place of residence in the US, divided into four regularly
+                smokes tobacco. geographic regions: northeast, southeast, southwest, or northwest."
+              )
+              )
+              )
+          ),
+        tags$br(),
+        "The application is built with the" ,
+        tags$a("Shiny", href = "https://shiny.rstudio.com/"),
+        " framework for the R programming language.Multiple Linear Regression algorithm was used to fit the model."
+        ,
+        tags$br(),
+        tags$br(),
+        tags$p(
+          "Code for the application is available at https://github.com/sdiwash/Electric-Dashboard.I welcome feedback and suggestions! Please visit my ",
+          tags$a("personal website", href = "https://diwashrestha.com/"),
+          "for contact information or connect with me on",
+          tags$a("Twitter.", href = "https://twitter.com/diwastha"),
+          " "
+        ),
+        
+        tags$div()
       )
-    )
+      
+      
+      
+  ))
   )
-))
+    ))
